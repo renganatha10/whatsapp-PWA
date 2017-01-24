2abcb16de0f289dc0441898ced3b9c5a21c89076
Renganatha <renganatha@sahusoft.com>
2017-01-25 02:29:36 +0530

    Initial Layout of app to all size screen

---
+++ app/components/chats/chatRow.js
@@ -0,0 +1,12 @@
+import React,{ Component } from 'react';
+
+export default class ChatRow extends Component {
+
+  render() {
+    return (
+        <div className="class-name">
+            content
+        </div>
+    );
+  }
+}
