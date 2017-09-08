e559c2c1c3f11a8af1bfb7356d8e5e57e868a393
Renganatha <renganatha@sahusoft.com>
2017-09-08 13:21:14 +0530

    Resolve Preact issue

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -32,7 +32,8 @@ module.exports = {
   resolve: {
     alias: {
       react: 'preact-compat',
-      'react-dom': 'preact-compat'
+      'react-dom': 'preact-compat',
+      'create-react-class': 'preact-compat/lib/create-react-class'
     }
   },
   plugins: [
