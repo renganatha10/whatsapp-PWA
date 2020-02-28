60ebfd4398af5995560bcc8fdd3d8a3123019b09
Renganatha Arunachalam <renganatha10@gmail.com>
2020-02-29 00:48:27 +0530

    Add Tailwind CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,11 @@ const Home = () => (
         </a>
       </div>
       <Button />
+      <input
+        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+        type="email"
+        placeholder="jane@example.com"
+      />
     </main>
     <footer>
       <a
