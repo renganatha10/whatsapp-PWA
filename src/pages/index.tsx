2839200f4c6a5794184b38b7c71e670587ee10eb
Renganatha Arunachalam <renganatha10@gmail.com>
2020-03-08 23:11:39 +0530

    Fix CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,16 @@ import Groups from "../containers/Groups";
 
 const Home = () => {
   return (
-    <div className="main bg-purple-500 px-16">
+    <div className="main bg-purple-500 lg:px-16">
       <Head>
         <title>WhatsWeb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="container flex h-full bg-gray-300">
-        <div className="lg:w-1/3 md:w-full border-r h-full border-gray-400 border-solid">
+        <div className="lg:w-1/3 w-full border-r h-full border-gray-400 border-solid">
           <Groups />
         </div>
-        <div className="mx-40 lg:block md:hidden md:w-2/3 h-full">
+        <div className="lg:mx-40 lg:block hidden lg:w-2/3 lg:h-full">
           <div className="flex h-full">
             <img src="./icons/group_chat.svg" alt="GroupChat" />
           </div>
